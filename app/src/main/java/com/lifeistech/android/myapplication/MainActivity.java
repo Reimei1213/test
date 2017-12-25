d200fcf7b668ffac4f62ec63d38e9ff5590561c8
Reimei1213 <kakibo1213@gmail.com>
2017-12-25 13:28:33 +0900

    変えました

--- app/src/main/java/com/lifeistech/android/myapplication/MainActivity.java
+++ app/src/main/java/com/lifeistech/android/myapplication/MainActivity.java
@@ -2,12 +2,19 @@ package com.lifeistech.android.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView);
+        text.setText("0");
+
     }
 }
